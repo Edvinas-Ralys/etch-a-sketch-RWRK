04fe2e6a132ec9867939c0e0ba364d0330965fc8
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-10-10 19:19:06 +0300

    .

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 const gridSizeOption = document.querySelector(`.gridSizeOption`);
 const sketch = document.querySelector(`.sketch`);
-const colorOption = document.querySelector(`.color`);
+const colorOption = document.querySelector(`.colorBox`);
+
+const red = document.createElement(`div`)
+red.classList.add(`red`);
+colorOption.appendChild(red);
+const green = document.createElement(`div`);
+green.classList.add(`green`);
+colorOption.appendChild(green);
+const blue = document.createElement(`div`);
+blue.classList.add(`blue`);
+colorOption.appendChild(blue);
 
 for(let i = 1; i < 10; i++){
 const gridSizeBox = document.createElement(`div`);
@@ -10,12 +20,20 @@ gridSizeBox.addEventListener(`click`,  function (){drawGrid(i)});
 gridSizeOption.appendChild(gridSizeBox);
 };
 
-for(let u = 0; u < 10; u++){
+for(let u = 1; u < 10; u++){
     const colorBox = document.createElement(`div`);
-    colorBox.classList.add(`color${u}`);
-    colorBox.innerHTML = `color ${u}`;
+    colorBox.classList.add(`color`);
+    if(u < 4){
+        colorBox.style.cssText = `background: rgb(${u*85},0,0);`;
+    }
+    else if(3 < u && u < 7){
+        colorBox.style.cssText = `background: rgb(0,${(u-3)*85},0);`;
+    }
+    else if (u > 6 && u < 10) {
+        colorBox.style.cssText = `background: rgb(0,0,${(u-6)*85});`;
+    }
+    colorOption.appendChild(colorBox)
     colorBox.addEventListener(`click`, function(){colorSelect(u)})
-    colorOption.appendChild(colorBox);
 }
 
 function drawGrid(tileCount){
@@ -36,23 +54,29 @@ while(j < tileCount*10){
         square.classList.add(`square`);
         column.appendChild(square);
     k++
-}
-}
-};
+}}};
+
+
 function colorSelect(u){
-    console.log(u)
     let tiles = document.querySelectorAll(`.square`);
-    if(u === 0){
+    if(u < 4){
         for(const tile of tiles){
             tile.addEventListener(`mouseover`, function(){
-                tile.style.cssText = `background: black;`;})
+                tile.style.cssText = `background: rgb(${u*85},0,0);`;})
             }
     }
-    else{
+    else if(3 < u && u < 7){
+        console.log(u)
         for(const tile of tiles){
             tile.addEventListener(`mouseover`, function(){
-                tile.style.cssText = `background: red;`;})
-            } 
+                tile.style.cssText = `background: rgb(0,${(u-3)*85},0);`;})
+            }
     }
-    };
+    else if(u > 6) {
+        for(const tile of tiles){
+            tile.addEventListener(`mouseover`, function(){
+                tile.style.cssText = `background: rgb(0,0,${(u-6)*85});`;})
+            }
+            
+    }};
 
